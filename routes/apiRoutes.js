7ddaec4b3c747b8c2add04100e1d137ce44e6c9c
jaredbucko <jaredbucko@icloud.com>
2020-01-21 12:08:57 -0800

    added app.delet api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,14 @@
-const journal = require("../journal");
+const notes = require("../notes");
 const fs = require("fs");
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
-    res.json(journal);
+    res.json(notes);
   });
 
   app.post("/api/notes", function(req, res) {
     let newArray = [];
-    const notes = fs.readFileSync('journal.json');
+    const notes = fs.readFileSync('notes.json');
     if (notes.length>0) {
       newArray = JSON.parse(notes);
     };
@@ -19,7 +19,23 @@ module.exports = function(app) {
     };
     newArray.push(newNote);
     const string = JSON.stringify(newArray);
-    fs.writeFileSync('journal.json', string);
-    res.json(journal);
+    fs.writeFileSync('notes.json', string);
+    res.json(notes);
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    const updatedArray = [];
+    const removeNote = req.params.id - 1;
+    let oldNotes = fs.readFileSync('notes.json');
+    oldNotes = JSON.parse(oldNotes);
+    oldNotes.splice(removeNote, 1);
+    for (let i=0; i<oldNotes.length; i++) {
+      oldNotes[i].id = i + 1;
+      updatedArray.push(oldNotes[i]);
+    };
+    const newString = JSON.stringify(updatedArray);
+    console.log(newString);
+    fs.writeFileSync('notes.json', newString);
+    res.json(notes);
   });
 };
